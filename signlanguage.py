5560be3b1e7c620cad4eaddf6bd8802a5c6bc5ac
Sarash Sahu <72274539+sarashsahu@users.noreply.github.com>
2023-07-09 18:40:28 +0530

    Initial commit

---
+++ signlanguage.py
@@ -0,0 +1,90 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+import time
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+classifier = Classifier("1500/keras_model.h5", "1500/labels.txt")
+offset = 20
+imgSize = 300
+labels = ["A", "B", "C", "D",
+          "E", "F", "G", "H",
+          "I", "J", "K", "L",
+          "M", "N", "O", "P",
+          "Q", "R", "S", "T",
+          "U", "V", "W", "X",
+          "Y", "Z"]
+text = ""
+last_recognition_time = time.time()
+
+def erase_last_capture():
+    global text
+    if len(text) > 0:
+        text = text[:-1]
+
+while True:
+    success, img = cap.read()
+    imgOutput = np.zeros((480, 1280, 3), np.uint8)
+    imgOutput[:, :640, :] = img.copy()
+
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+        imgCropShape = imgCrop.shape
+
+        aspectRatio = h / w
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+        cv2.rectangle(imgOutput, (x - offset, y - offset - 50),
+                      (x - offset + 200, y - offset - 50 + 50), (255, 0, 255), cv2.FILLED)
+        cv2.putText(imgOutput, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+        cv2.rectangle(imgOutput, (x - offset, y - offset),
+                      (x + w + offset, y + h + offset), (255, 0, 255), 4)
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+
+        if time.time() - last_recognition_time > 3:
+            text += labels[index]
+            last_recognition_time = time.time()
+            if cv2.getTextSize(text, cv2.FONT_HERSHEY_COMPLEX, 0.9, 2)[0][0] > 1000:
+                break
+
+    cv2.putText(imgOutput, "CAPTURED TEXT:", (660, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+    cv2.putText(imgOutput, text, (660, 100), cv2.FONT_HERSHEY_COMPLEX, 0.9, (255, 0, 255), 2)
+
+    cv2.imshow("Image", imgOutput)
+
+    key = cv2.waitKey(1)
+    if key == ord('c') or key == ord('C'):
+        text = ""
+    elif key == ord('f') or key == ord('F'):
+        erase_last_capture()
+
+    if key == ord('q') or key == ord('Q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
